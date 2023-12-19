93baceafd6a5e7c7faecd2b651ec5879647c200a
Smit Joshi <104095303+s-skywalker@users.noreply.github.com>
2023-12-19 23:07:46 +0530

    Update uniquearray.cpp

--- Arrays/uniquearray.cpp
+++ Arrays/uniquearray.cpp
@@ -19,7 +19,12 @@ int n = 7;
 
 int finalAns = uniqueArray (arr, n);
 cout<<"Final ans is : "<<finalAns<<endl;
-
+// array 2,1,0,0,2,1,5 aahe
+// we are using the XOR operator for finding the unique element. 
+// the logic cancel outs the same elements such as 2, 1 and 0 with each other 
+// XOR 5 with 0 th index, at 0 th index we have 2, so start XORING with 2 (it works like this. ans =  0 ^ 2 ^ 1 ^ 0 ^ 0 ^ 2 ^ 1 ^ 5) 
+// ^ = XOR Operator 
+// now, the unique element in the array is 5, so it is XOR'ed with 0, and any number XOR'ed with 0 returns the same number, so we print the unique element 
 
     return 0;
 }
